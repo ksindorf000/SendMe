b3042c8f752b859ac4207876ddb5378b6e9844dd
ksindorf000 <ksindorf000@gmail.com>
2017-03-22 15:30:11 -0400

    Added receipt emails to donations

--- SendMe/Controllers/DonationController.cs
+++ SendMe/Controllers/DonationController.cs
@@ -1,7 +1,9 @@
-﻿using SendMe.Models;
+﻿using SendMe.Helpers;
+using SendMe.Models;
 using Stripe;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -95,14 +97,9 @@ namespace SendMe.Controllers
                             db.Entry(trip).State = System.Data.Entity.EntityState.Modified;
                             db.SaveChanges();
                         }
-                        //var myMessage = new SendGrid.SendGridMessage();
-                        //myMessage.AddTo("test@sendgrid.com");
-                        //myMessage.From = new MailAddress("you@youremail.com", "First Last");
-                        //myMessage.Subject = "Sending with SendGrid is Fun";
-                        //myMessage.Text = "and easy to do anywhere, even with C#";
 
-                        //var transportWeb = new SendGrid.Web("SENDGRID_APIKEY");
-                        //transportWeb.DeliverAsync(myMessage);
+                        SendReceiptEmail(Email, amount, Name, tripId);
+
                     }
                     else
                     {
@@ -114,6 +111,37 @@ namespace SendMe.Controllers
             return RedirectToAction(userName, new RouteValueDictionary(
             new { controller = "send", action = userName, paymentMsg = paymentMessage, email = Email}));
 
+        }
+
+        private static void SendReceiptEmail(string toEmail, int? amount, string name, int? tripId)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            Trip trip = db.Trips
+                .SingleOrDefault(t => t.Id == tripId);
+
+            DateTime date = DateTime.Today;           
+            string dateFormat = "MMM d yyyy";
+            string today = date.ToString(dateFormat);
+
+            StuProfile student = trip.Student;
+            StudentViewModel stuVM = new StudentViewModel(student);
+
+            string picPath = stuVM.Upload.FilePath;
+
+            string msg = "Thank you for helping send "
+                + stuVM.Student.FirstName + " to " + trip.Destination + "! </br>"
+                + "You donated $" + amount + " on " + today + ".";
+
+            string body = "<table><tr><td style=\"padding: 20px\"><img src=\"" + picPath
+                + "\" style = \"width: 150px; height: 150px; border-radius: 50%\" ></td >"
+                + "<td style=\"padding: 20px: text-align: left\">" + msg + "</td></tr></table>";
+
+            string fromEmail = ConfigurationManager.AppSettings["SendEmailsFrom"];
+
+            string subj = "Donation Receipt from SendMe!";
+
+            MailHelper.Execute(body, name, toEmail, "SendMe!", fromEmail, subj);
         }
     }
 }
